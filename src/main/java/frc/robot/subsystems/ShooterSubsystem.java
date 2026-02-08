0ba7ae01c7bd4d44821a0ce263de413ab2874067
OriZamir1 <orizamir4@gmail.com>
2026-02-08 21:52:14 +0200

    added PID and FF

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,9 +4,13 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Degrees;
+
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -18,6 +22,16 @@ public class ShooterSubsystem extends SubsystemBase {
 
   private CANcoder hoodCancoder;
   private CANcoder turretCancoder;
+
+  private ProfiledPIDController hoodPID;
+  private ProfiledPIDController turretPID;
+
+  private SimpleMotorFeedforward hoodFF;
+  private SimpleMotorFeedforward turretFF;
+
+  private double turretTolerence = 5;
+  private double hoodTolerence = 3;
+
   public ShooterSubsystem() {
     this.turretMotor = new TalonFX(0);
     this.flyWheelMotor = new TalonFX(0);
@@ -25,6 +39,15 @@ public class ShooterSubsystem extends SubsystemBase {
 
     this.hoodCancoder = new CANcoder(0);
     this.turretCancoder = new CANcoder(0);
+
+    this.hoodPID = new ProfiledPIDController(0, 0, 0, null);
+    this.hoodPID.setTolerance(hoodTolerence);
+    this.turretPID = new ProfiledPIDController(0, 0, 0, null);
+    this.turretPID.setTolerance(hoodTolerence);
+
+    this.hoodFF = new SimpleMotorFeedforward(0, 0, 0);
+    this.turretFF = new SimpleMotorFeedforward(0, 0, 0);
+
   }
   public Angle getHoodAngle(){
     return this.hoodCancoder.getAbsolutePosition().getValue();
@@ -33,8 +56,27 @@ public class ShooterSubsystem extends SubsystemBase {
     return this.turretCancoder.getAbsolutePosition().getValue();
   }
 
+  public void setTurretAngle(double deg){
+    this.turretPID.setGoal(deg);
+  }
+  public void setHoodAngle(double deg){
+    this.hoodPID.setGoal(deg);
+  }
+
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+  // This method will be called once per scheduler run
+  
+  // Calculate PID output
+  double hoodPIDOutput = hoodPID.calculate(this.getHoodAngle().in(Degrees));
+  double turretPIDOutput = turretPID.calculate(this.getTurrentAngle().in(Degrees));
+  
+  // Calculate feedforward output using the setpoint velocity
+  double hoodFFOutput = hoodFF.calculate(hoodPID.getSetpoint().velocity);
+  double turretFFOutput = turretFF.calculate(turretPID.getSetpoint().velocity);
+  
+  // Combine PID and feedforward outputs
+  this.hoodMotor.set(hoodPIDOutput + hoodFFOutput);
+  this.turretMotor.set(turretPIDOutput + turretFFOutput);
   }
 }
